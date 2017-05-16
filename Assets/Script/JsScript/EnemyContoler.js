56550e79ee8845ab31a7efba45f363c7b07558a5
Samiran <samirankumar11@gmail.com>
2017-05-16 17:49:39 +0600

    first commit 2

---
+++ Assets/Script/JsScript/EnemyContoler.js
@@ -0,0 +1,10 @@
+﻿var EnemyHealth : int = 10;
+function DeductPoint(DamageAmount : int){
+	EnemyHealth -= DamageAmount; //DamageAmount
+}
+
+function Update () {
+	if(EnemyHealth <= 0){
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Script/JsScript/HandGunDamage.js
@@ -0,0 +1,15 @@
+﻿var DamageAmount : int = 5;
+var TargetDistances : float ; 
+var AllowedRange : float = 15;
+
+function Update () {
+	 if(Input.GetButtonDown("Fire1")) {
+	 	var Shot : RaycastHit;
+	 	if(Physics.Raycast (transform.position,transform.TransformDirection(Vector3.forward) , Shot)){
+	 	 TargetDistances = Shot.distance;
+	 		if(TargetDistances < AllowedRange){
+	 			Shot.transform.SendMessage("DeductPoint",DamageAmount,SendMessageOptions.DontRequireReceiver);
+	 		}
+	 	}
+	 }
+}
