95d283781163a0445280b4881fa6b5ca4803d312
SamiranKumar <samirankumar11@gmail.com>
2017-05-22 15:50:20 +0600

    add flying bird

--- Assets/Script/JsScript/EnemyContoler.js
+++ Assets/Script/JsScript/EnemyContoler.js
@@ -1,10 +1,12 @@
 ﻿var EnemyHealth : int = 10;
 function DeductPoint(DamageAmount : int){
 	EnemyHealth -= DamageAmount; //DamageAmount
+	print("hit");
 }
 
 function Update () {
 	if(EnemyHealth <= 0){
 		Destroy(gameObject);
+		
 	}
 }
---
+++ Assets/Script/MoveBird.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBird : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+    public Vector3 tempPosition;
+    // Use this for initialization
+    void Start () {
+        tempPosition = transform.position;
+       // horizontalSpeed = tempPosition.x;
+    verticalSpeed = tempPosition.y;
+        
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude +3;
+
+        transform.position = tempPosition;
+    }
+}
